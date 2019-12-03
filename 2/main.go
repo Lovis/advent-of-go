387b13fde3ce39ff9553e6835317e54a8d5edbc0
Lovis Dahl <lovi.dahl@gmail.com>
2019-12-03 23:06:32 +0100

    make it work

--- 2/main.go
+++ 2/main.go
@@ -2,16 +2,24 @@ package main
 
 import "fmt"
 
-var data = []int{1,1,1,4,99,5,6,0,99}
+var data = []int{1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,23,6,27,2,9,27,31,1,5,31,35,1,35,10,39,1,39,10,43,2,43,9,47,1,6,47,51,2,51,6,55,1,5,55,59,2,59,10,63,1,9,63,67,1,9,67,71,2,71,6,75,1,5,75,79,1,5,79,83,1,9,83,87,2,87,10,91,2,10,91,95,1,95,9,99,2,99,9,103,2,10,103,107,2,9,107,111,1,111,5,115,1,115,2,119,1,119,6,0,99,2,0,14,0}
 
 
 
 func main() {
+
+	data[1] = 12
+	data[2] = 2
+
 	fmt.Println(data)
 	i := 0
 	for {
 		fmt.Println(i)
 		if (i >= len(data)) {
+			break
+		}
+		if (data[i] == 99) {
+			fmt.Println("found a 99:", i)
 			break
 		}
 		if (data[i] == 1 || data[i] == 2) {
@@ -30,10 +38,6 @@ func main() {
 				continue
 			}
 		}
-		if (i == 99) {
-			fmt.Println("found a 99:", i)
-			break
-		}
 		
 		if (i < len(data)) {
 			fmt.Println("reached end, incrementing by 1")
@@ -43,5 +47,6 @@ func main() {
 
 
 	fmt.Println(data)
+	fmt.Println(data[0])
 }
 
