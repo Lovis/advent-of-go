3bf1cbdb25d4df6138cc807a680195cc2fddc57c
Lovis Dahl <lovi.dahl@gmail.com>
2019-12-08 12:47:23 +0100

    add manhattan calc

--- 3/main.go
+++ 3/main.go
@@ -15,21 +15,43 @@ type Coords struct {
 	x, y int
 }
 
+func calcDistance(coords []Coords) (smallest int) {
+	// traverse the list, find the smallest
+	smallest = coords[0].x + coords[0].y
+	for _, v := range coords {
+		manhattan := v.x + v.y
+		if (manhattan < smallest) {
+			smallest = manhattan
+		}
+	}
+	return smallest
+}
+
+func findIntersections(left []Coords, right []Coords) []Coords {
+	var intersections []Coords
+	for i, v := range left {
+		fmt.Println(i, v, right[i])
+		if (v.x == right[i].x && v.y == right[i].y) {
+			intersections = append(intersections, v)
+		}
+	}
+	return intersections
+}
 
 func parse(input []string, coords []Coords) ([]Coords) {
 	// parse and create array of coords
-	for i := 0; i < 2; i++ {
-		fmt.Println("first loop", i, len(input))
+	for i := 0; i < len(input); i++ {
+		// fmt.Println("first loop", i, len(input))
 		var instruction = input[i]
-		fmt.Println("instruction", instruction)
+		// fmt.Println("instruction", instruction)
 		var direction = string(instruction[0])
 		steps, err := strconv.Atoi(string(instruction[1]))
 		if err != nil {
 			break
 		}
-		fmt.Println("directions", direction, "steps", steps, "unparsed", instruction[1])
+		// fmt.Println("directions", direction, "steps", steps, "unparsed", instruction[1])
 		for j := 0; j < steps; j++ {
-			fmt.Println("here", i, "and", i)
+			// fmt.Println("here", i, "and", i)
 			var x = 0
 			var y = 0
 			if (len(coords) > 0) {
@@ -48,7 +70,7 @@ func parse(input []string, coords []Coords) ([]Coords) {
 			}
 
 			newCoord := Coords{x,y}
-			fmt.Println("new coord", newCoord)
+			// fmt.Println("new coord", newCoord)
 			coords = append(coords, newCoord)
 		}
 	}
@@ -57,7 +79,16 @@ func parse(input []string, coords []Coords) ([]Coords) {
 func main() {
 	fmt.Println("here we go")
 	coordsLeft = parse(left, coordsLeft)
-	fmt.Println("coords: ", coordsLeft)
+	fmt.Println("coordsLeft: ", coordsLeft)
+
+	coordsRight = parse(right, coordsRight)
+	fmt.Println("coordsRight: ", coordsRight)
+
+	intersections := findIntersections(coordsLeft, coordsRight)
+	fmt.Println("intersections", intersections)
+
+	smallest := calcDistance(intersections)
+	fmt.Println("smallest manhattan: ", smallest)
 	// extend to two full arrays of cells (x,y)
 	// find the common cells
 	// measure how far they are from origo
