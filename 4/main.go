04416738e23e48249254309e31ed7e8483bac78a
Lovis Dahl <lovi.dahl@gmail.com>
2019-12-08 15:39:54 +0100

    carefully look for a tuple not a group

--- 4/main.go
+++ 4/main.go
@@ -14,12 +14,12 @@ type Limit struct {
 func part2matching(number int) (bool) {
 	converted := strconv.Itoa(number)
 	hasDouble := false
+
 	for i := 0; i < len(converted); i++ {
 		// rule 1: increasing
 		// fmt.Println(i, string(converted[i]))
 		if (i > 0 && string(converted[i]) < string(converted[i-1])) {
-			hasDouble = false
-			// return hasDouble
+			return false
 		}
 
 		// rule 2: mnultiple occurrencies
@@ -28,8 +28,6 @@ func part2matching(number int) (bool) {
 		// fmt.Println(sep, length)
 		if (length == 3) {
 			hasDouble = true
-		} else if (length > 3) {
-			hasDouble = false
 		}
 
 	}
@@ -78,9 +76,10 @@ func main() {
 	// }
 	// fmt.Println("number of combinations", count)
 
-	// fmt.Println("has multi?", part2matching(112233))
-	// fmt.Println("has multi?", part2matching(123444))
-	// fmt.Println("has multi?", part2matching(111122))
+	fmt.Println("has multi?", part2matching(112233))
+	fmt.Println("has multi?", part2matching(123444))
+	fmt.Println("has multi?", part2matching(111122))
+	fmt.Println("has multi?", part2matching(224444))
 
 	count := 0
 	for i := config.lower; i < config.upper; i++ {
