8879f07e6c9e4cfffab7071f847f1a508b8b7a46
Lovis Dahl <lovi.dahl@gmail.com>
2019-12-08 15:39:33 +0100

    get going w pt2

--- 4/main.go
+++ 4/main.go
@@ -11,6 +11,31 @@ type Limit struct {
 	lower, upper int
 }
 
+func part2matching(number int) (bool) {
+	converted := strconv.Itoa(number)
+	hasDouble := false
+	for i := 0; i < len(converted); i++ {
+		// rule 1: increasing
+		// fmt.Println(i, string(converted[i]))
+		if (i > 0 && string(converted[i]) < string(converted[i-1])) {
+			hasDouble = false
+			// return hasDouble
+		}
+
+		// rule 2: mnultiple occurrencies
+		sep := string(converted[i])
+		length := len(strings.Split(converted, sep))
+		// fmt.Println(sep, length)
+		if (length == 3) {
+			hasDouble = true
+		} else if (length > 3) {
+			hasDouble = false
+		}
+
+	}
+	return hasDouble
+}
+
 func hasDouble(number int) (bool) {
 	converted := strconv.Itoa(number)
 	hasDouble := false
@@ -34,19 +59,32 @@ func hasDouble(number int) (bool) {
 }
 
 func main() {
-	// fmt.Println("Puzzle 4")
+	fmt.Println("Puzzle 4")
+	config := Limit{136760, 595730}
 	// fmt.Println("has double? ", hasDouble(123456))
 	// fmt.Println("has double? ", hasDouble(123356))
 	// fmt.Println("has double? ", hasDouble(153356))
 
 	// fmt.Println("has double? ", hasDouble(111111))
 	// fmt.Println("has double? ", hasDouble(223450))
-	// fmt.Println("has double? ", hasDouble(123789))
-	config := Limit{136760, 595730}
+	// // fmt.Println("has double? ", hasDouble(123789))
+
+
+	// count := 0
+	// for i := config.lower; i < config.upper; i++ {
+	// 	if (hasDouble(i)) {
+	// 		count++
+	// 	}
+	// }
+	// fmt.Println("number of combinations", count)
+
+	// fmt.Println("has multi?", part2matching(112233))
+	// fmt.Println("has multi?", part2matching(123444))
+	// fmt.Println("has multi?", part2matching(111122))
 
 	count := 0
 	for i := config.lower; i < config.upper; i++ {
-		if (hasDouble(i)) {
+		if (part2matching(i)) {
 			count++
 		}
 	}
