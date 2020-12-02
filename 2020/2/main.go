7cc64b2b8c60f95706f271959cd7b174a5bd20ed
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-02 22:37:17 +0100

    read into struct

--- 2020/2/main.go
+++ 2020/2/main.go
@@ -5,11 +5,19 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
+
+type PasswordPolicy struct {
+	lower      int
+	upper      int
+	letter     string
+	testPhrase string
+}
 
 func main() {
 
-	file, err := os.Open("sample.txt")
+	file, err := os.Open("input.txt")
 
 	if err != nil {
 		fmt.Printf("error opening file")
@@ -19,15 +27,23 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	scanner.Split(bufio.ScanLines)
-	var input []int
+	var input []PasswordPolicy
 
 	for scanner.Scan() {
-		i, err := strconv.Atoi(scanner.Text())
+		s := strings.Split(scanner.Text(), " ")
 
-		if err != nil {
-			fmt.Printf("oops, error")
-		}
-		input = append(input, i)
+		var policy PasswordPolicy
+
+		policy.letter = strings.Trim(s[1], ":")
+		policy.testPhrase = s[2]
+
+		limits := strings.Split(s[0], "-")
+
+		policy.lower, _ = strconv.Atoi(limits[0])
+		policy.upper, _ = strconv.Atoi(limits[1])
+
+		input = append(input, policy)
+
 	}
 	fmt.Printf("Day 2, going steady \n")
 	fmt.Printf("input: %v \n", input)
