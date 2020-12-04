2974f325d9028060a09b444712320007f934a984
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-04 18:39:01 +0100

    valid works

--- 2020/4/main.go
+++ 2020/4/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -19,23 +21,105 @@ import (
 // }
 
 // break on empty line
+var eyeColors = []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
 
-func validPassport(passport []string) bool {
+func ecl(color string) bool {
+	var valid = false
+	for _, eyeColor := range eyeColors {
+		if color == eyeColor {
+			valid = true
+			break
+		}
+	}
+	return valid
+}
+
+func verifiedPassport(passport []string) bool {
 	var cid = false
+	var presentParameters = false
+	var validParameters = true
+
 	for _, attribute := range passport {
-		if strings.Split(attribute, ":")[0] == "cid" {
+		keyvalue := strings.Split(attribute, ":")
+		key, value := keyvalue[0], keyvalue[1]
+
+		switch key {
+		case "byr":
+			year, _ := strconv.Atoi(value)
+			// fmt.Printf("year %d %t %t\n", year, 1920 <= year, 2002 >= year)
+			if !(1920 <= year && 2002 >= year) {
+				validParameters = false
+				break
+			}
+
+		case "iyr":
+			year, _ := strconv.Atoi(value)
+			if !(2010 <= year && 2020 >= year) {
+				validParameters = false
+				break
+			}
+
+		case "eyr":
+			year, _ := strconv.Atoi(value)
+			if !(2020 <= year && 2030 >= year) {
+				validParameters = false
+				break
+			}
+
+		case "hgt":
+			if strings.Contains(value, "in") {
+				s := strings.Split(value, "in")
+				var height, _ = strconv.Atoi(s[0])
+				if !(59 <= height && 76 >= height) {
+					validParameters = false
+					break
+				}
+			} else if strings.Contains(value, "cm") {
+				s := strings.Split(value, "cm")
+				var height, _ = strconv.Atoi(s[0])
+				if !(150 <= height && 193 >= height) {
+					validParameters = false
+					break
+				}
+			}
+
+		case "hcl":
+			matched, _ := regexp.MatchString(`^#[a-f0-9]{6}$`, value)
+			if !matched {
+				validParameters = false
+				break
+			}
+
+		case "ecl":
+			validEyeColor := ecl(value)
+			if !validEyeColor {
+				validParameters = false
+				break
+			}
+
+		case "pid":
+			matched, _ := regexp.MatchString(`^[0-9]{0}`, value)
+			if !matched {
+				validParameters = false
+				break
+			}
+		}
+
+		if key == "cid" {
 			cid = true
 			continue
 		}
 	}
-	// fmt.Printf("valid? %d %t\n", len(passport), cid)
-	return len(passport) == 8 || (len(passport) == 7 && !cid)
+
+	presentParameters = len(passport) == 8 || (len(passport) == 7 && !cid)
+	fmt.Printf("present %t\n", presentParameters)
+	return validParameters
 }
 
 func main() {
 	fmt.Printf("Day 4\n")
 
-	file, err := os.Open("input.txt")
+	file, err := os.Open("valid.txt")
 
 	if err != nil {
 		fmt.Printf("error opening file")
@@ -54,7 +138,7 @@ func main() {
 			// end of passport.
 			// verify length.
 			// if length is less than 1, look
-			if validPassport(passport) {
+			if verifiedPassport(passport) {
 				passports = append(passports, passport)
 			}
 			passport = []string{}
