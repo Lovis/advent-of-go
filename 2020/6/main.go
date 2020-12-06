9d8e6fa897ff21d25605d57407a4053d0cda6a6f
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-06 09:34:08 +0100

    part 2

--- 2020/6/main.go
+++ 2020/6/main.go
@@ -31,6 +31,41 @@ func findAnyAffirmativeAnswer(scanner *bufio.Scanner) int {
 	return len(anyInGroupResponses)
 }
 
+func findCommonAffirmativeAnswer(scanner *bufio.Scanner) int {
+	// each group is separated by blank line
+	// each person, within the group is on a separate line
+	var commonAffirmativeAnswers = ""
+	var groupResponses = ""
+	var inProcess = false
+
+	for scanner.Scan() {
+		s := scanner.Text()
+		if s == "" {
+			// end of group
+			// fmt.Printf("summing, group unique: [%v]\n", groupResponses)
+			commonAffirmativeAnswers += groupResponses
+			groupResponses = ""
+			inProcess = false
+		} else if groupResponses == "" && !inProcess {
+			groupResponses = s
+			// fmt.Printf("\nfirst in new group: [%v]\n", groupResponses)
+		} else {
+			// groupResponses: abcx
+			// s: aex
+			// traverse groupResponses, if not existing
+			// fmt.Printf("traversing: group: [%v], s [%s]\n", groupResponses, s)
+			inProcess = true
+			for _, char := range groupResponses {
+				charString := string(char)
+				if !strings.Contains(s, charString) {
+					groupResponses = strings.ReplaceAll(groupResponses, charString, "")
+				}
+			}
+		}
+	}
+	return len(commonAffirmativeAnswers)
+}
+
 func main() {
 	fmt.Printf("Day 6\n")
 
@@ -47,6 +82,9 @@ func main() {
 
 	scanner.Split(bufio.ScanLines)
 
-	var anyInGroupResponsesTotal = findAnyAffirmativeAnswer(scanner)
-	fmt.Printf("number of right answers: %d\n", anyInGroupResponsesTotal)
+	// var anyInGroupResponsesTotal = findAnyAffirmativeAnswer(scanner)
+	// fmt.Printf("PART 1, number of right answers: %d\n", anyInGroupResponsesTotal)
+
+	var commonInGroupResponsesTotal = findCommonAffirmativeAnswer(scanner)
+	fmt.Printf("PART 2, number of right answers: %d\n", commonInGroupResponsesTotal)
 }
