ff30bd0c95c94e438b635ccc2f09e4a06bb7561e
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-09 13:13:34 +0100

    stash state

--- 2020/8/main.go
+++ 2020/8/main.go
@@ -41,7 +41,7 @@ func readInstructions(instructions []Instruction) int {
 	var index = 0
 	for !found {
 		if index == len(instructions) {
-			fmt.Printf("this is success!!\n")
+			fmt.Printf("reached the last line!\n")
 			found = true
 			break
 		}
@@ -50,8 +50,9 @@ func readInstructions(instructions []Instruction) int {
 		fmt.Printf("visiting [%v]\n", instruction)
 
 		if instruction.executed == true {
-			fmt.Printf("second visit! for index [%d]: [%v]", index, instruction)
+			fmt.Printf("second visit! for index [%d]: [%v]\n\n", index, instruction)
 			found = true
+			acc = -1 // remove this line for pt1
 			break
 
 		} else if instruction.operation == "acc" {
@@ -72,10 +73,46 @@ func readInstructions(instructions []Instruction) int {
 	return acc
 }
 
+// traverse the instructions, and carefully shift one jmp or nop at the time
+
+func traverseAndShiftInstructions(instructions []Instruction) int {
+	var swapIndex = 0
+	var acc = -1
+	var masterData = instructions
+	for index, instruction := range masterData {
+		if instruction.operation == "jmp" || instruction.operation == "nop" {
+			swapIndex = index
+			switch instruction.operation {
+			case "nop":
+				instruction.operation = "jmp"
+
+			case "jmp":
+				instruction.operation = "nop"
+			}
+			fmt.Printf("swapping %d\n", swapIndex)
+			acc := readInstructions(masterData)
+			if acc != -1 {
+				fmt.Printf("made it all the way! swapping %d: acc %d\n", swapIndex, acc)
+				break
+			} else {
+				fmt.Printf("no success swapping %d %v\n", swapIndex, instruction)
+				switch instructions[swapIndex].operation {
+				case "nop":
+					instructions[swapIndex].operation = "jmp"
+
+				case "jmp":
+					instructions[swapIndex].operation = "nop"
+				}
+			}
+		}
+	}
+	return acc
+}
+
 func main() {
 	fmt.Printf("Day 8\n")
 
-	file, err := os.Open("input.txt")
+	file, err := os.Open("tiny.txt")
 
 	if err != nil {
 		fmt.Printf("error opening file\n")
@@ -87,6 +124,6 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 
 	var instructions = parseInstructions(scanner)
-	var accValue = readInstructions(instructions)
+	var accValue = traverseAndShiftInstructions(instructions)
 	fmt.Printf("PART 1, acc: %d\n", accValue)
 }
