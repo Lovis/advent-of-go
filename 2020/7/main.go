25e50dbaa6ea332f48c573b4ef63762536187b01
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-09 16:12:38 +0100

    UGLY BUT WORKING

--- 2020/7/main.go
+++ 2020/7/main.go
@@ -49,6 +49,53 @@ func parseRules(scanner *bufio.Scanner) []Bag {
 	return bags
 }
 
+func findShinyBags(bags []Bag) int {
+	// use the []Bag
+	// traverse from every entry but the one starting w shiny bag
+	// when done, if shiny is in the list => +1
+
+	var containsShiny []int
+
+	for index, bag := range bags {
+		if bag.color != "shiny gold" {
+			metShiny := traverse(bags, bag, index)
+			if metShiny {
+				containsShiny = append(containsShiny, index)
+			}
+		}
+	}
+	return len(containsShiny)
+
+	// traverse(bags, bags[0])
+}
+
+func traverse(bags []Bag, current Bag, index int) bool {
+	var metShiny = false
+	if (len(current.contains) == 0 || metShiny) {
+		// now we're done.
+		fmt.Printf("reached shiny gold or no more bags\n")
+		return true
+	} else {
+		for _, sub := range current.contains {
+			if sub.color == "shiny gold" {
+				metShiny = true
+				fmt.Printf("met shiny, breaking\n")
+				break
+			} else {
+				// traverse further
+				fmt.Printf("here [%v]\n", sub)
+				// UGLY need to find the Bag holding sub.color
+				for _, next := range bags {
+					if sub.color == next.color {
+						traverse(bags, next, index)
+					}
+				}
+			}
+		}
+	}
+	return metShiny
+}
+
 func main() {
 	fmt.Printf("Day 7\n")
 
@@ -64,5 +111,5 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 
 	var rules = parseRules(scanner)
-	fmt.Printf("PART 1 %v\n", rules)
+	fmt.Printf("PART 1 %v\n", findShinyBags(rules))
 }
