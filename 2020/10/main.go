f4284db9a18a34c9bf2d05c4fb04af98d2caae3b
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-14 18:49:57 +0100

    parse

---
+++ 2020/10/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func parseInput(scanner *bufio.Scanner, preambleLimit int) []int {
+	var numbers []int
+	for scanner.Scan() {
+		var current int
+		s := scanner.Text()
+		if s == "" {
+			continue
+		}
+		current, _ = strconv.Atoi(s)
+		numbers = append(numbers, current)
+	}
+
+	return numbers
+}
+
+func main() {
+	fmt.Printf("Day 10\n")
+
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		fmt.Printf("error opening file\n")
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+
+	fmt.Printf("PART 1, joltage: %d\n", parseInput(scanner))
+}
