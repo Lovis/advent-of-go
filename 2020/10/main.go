6686ec85a330480bb435e2212f72751785129991
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-14 19:47:54 +0100

    part2 mini works

--- 2020/10/main.go
+++ 2020/10/main.go
@@ -33,6 +33,42 @@ func traverseAdapters(adapters []int) int {
 	return diff1 * (diff3)
 }
 
+// traverse the list count the number of options:
+//
+func distinctWaysOfTraveling(adapters []int) int {
+
+	// 1. convert to map for access by value
+	var adapterMap = make(map[int]int)
+
+	for _, a := range adapters {
+		adapterMap[a] = a
+	}
+
+	var ways []int
+	for value := range adapterMap {
+		// for every index
+		// 7 8 9 10
+		var options []int
+		if _, ok := adapterMap[value+1]; ok {
+			options = append(options, value+1)
+		}
+		if _, ok := adapterMap[value+2]; ok {
+			options = append(options, value+2)
+		}
+		if _, ok := adapterMap[value+3]; ok {
+			options = append(options, value+3)
+		}
+
+		if value != adapters[len(adapters)-1] && len(options) > 1 {
+			ways = append(ways, options...)
+			fmt.Printf("round: %d, adding: %d total %d\n", value, options, ways)
+		}
+
+	}
+
+	return len(ways) + 1
+}
+
 func parseInput(scanner *bufio.Scanner) []int {
 	var numbers []int
 	for scanner.Scan() {
@@ -52,7 +88,7 @@ func parseInput(scanner *bufio.Scanner) []int {
 func main() {
 	fmt.Printf("Day 10\n")
 
-	file, err := os.Open("input.txt")
+	file, err := os.Open("mini.txt")
 
 	if err != nil {
 		fmt.Printf("error opening file\n")
@@ -64,6 +100,9 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 
 	numbers := parseInput(scanner)
-	sum := traverseAdapters(numbers)
-	fmt.Printf("PART 1, sum %d\n", sum)
+	// sum := traverseAdapters(numbers)
+	// fmt.Printf("PART 1, sum %d\n", sum)
+
+	variations := distinctWaysOfTraveling(numbers)
+	fmt.Printf("PART 2, variations %d\n", variations)
 }
