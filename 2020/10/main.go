a96507b1e5244755a430ea298985d2c18bbf4a29
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-14 19:06:30 +0100

    tiny tiny works

--- 2020/10/main.go
+++ 2020/10/main.go
@@ -4,8 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
+
+// traverse the list of joltage, starting from 0
+// count the differences d
+func traverseAdapters(adapters []int) int {
+	var diff1 = 0
+	var diff3 = 0
+
+	var current = 0
+	for _, adapter := range adapters {
+		diff := adapter - current
+		fmt.Printf("diff %d adapter %d current %d\n", diff, adapter, current)
+		if diff == 1 {
+			fmt.Printf("+++ 1 steps adapter %d\n", adapter)
+			diff1++
+		}
+		if diff == 3 {
+			fmt.Printf("--- 3 steps adapter %d\n", adapter)
+			diff3++
+		}
+
+		current = adapter
+	}
+	fmt.Printf("diff1 %d diff3 %d\n", diff1, diff3)
+
+	// Finally, your device's built-in adapter is always 3 higher than the highest adapter
+	// so its rating is 22 jolts (always a difference of 3)
+	diff3 += 3
+	return diff1 * (diff3 + 1)
+}
 
 func parseInput(scanner *bufio.Scanner) []int {
 	var numbers []int
@@ -19,6 +49,7 @@ func parseInput(scanner *bufio.Scanner) []int {
 		numbers = append(numbers, current)
 	}
 
+	sort.Ints(numbers)
 	return numbers
 }
 
@@ -36,5 +67,7 @@ func main() {
 
 	scanner.Split(bufio.ScanLines)
 
-	fmt.Printf("PART 1, joltage: %d\n", parseInput(scanner))
+	numbers := parseInput(scanner)
+	sum := traverseAdapters(numbers)
+	fmt.Printf("PART 1, joltage: %v, sum %d\n", numbers, sum)
 }
