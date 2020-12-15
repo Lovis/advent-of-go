742cc612d808447c546b1401406409ba28f07918
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-15 22:51:02 +0100

    copying maps are tough

--- 11/main.go
+++ 11/main.go
@@ -2,14 +2,59 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
 )
 
-func parseInput() [][]string {
+func getAdjacentOccupiedSeats(cx int, cy int, seatmap map[int][]string) int {
+	occupiedSeats := 0
+
+	for x := cx - 1; x <= cx+1; x++ {
+		for y := cy - 1; y <= cy+1; y++ {
+			if y == cy && x == cx {
+				continue
+			}
+			if row, ok := seatmap[y]; ok {
+				if x >= 0 && x < len(row) {
+					neighbor := row[x]
+					if (neighbor) == "#" {
+						occupiedSeats++
+					}
+				}
+			}
+		}
+	}
+
+	return occupiedSeats
+}
+
+func simulate(seatmap map[int][]string) map[int][]string {
+	var simulated = make(map[int][]string)
+	jsonStr, _ := json.Marshal(seatmap)
+	json.Unmarshal(jsonStr, &simulated)
+
+	for i, row := range seatmap {
+		for k, seat := range row {
+			// check neighbors
+			occupiedSeats := getAdjacentOccupiedSeats(i, k, seatmap)
+			fmt.Printf("checking %d %d: neighbors: %d\n", i, k, occupiedSeats)
+			if seat == "L" && occupiedSeats == 0 {
+				simulated[i][k] = "#"
+				fmt.Printf("changing to # %d %d \n", i, k)
+			} else if seat == "#" && occupiedSeats >= 4 {
+				simulated[i][k] = "L"
+				fmt.Printf("changing to L %d %d \n", i, k)
+			}
+		}
+	}
+
+	return simulated
+}
+func parseInput() map[int][]string {
 	file, err := os.Open("tiny.txt")
-	var seatmap [][]string
+	var seatmap = make(map[int][]string)
 
 	if err != nil {
 		fmt.Printf("error opening file\n")
@@ -19,7 +64,7 @@ func parseInput() [][]string {
 	scanner := bufio.NewScanner(file)
 
 	scanner.Split(bufio.ScanLines)
-
+	index := 0
 	for scanner.Scan() {
 		var row []string
 		s := scanner.Text()
@@ -27,7 +72,8 @@ func parseInput() [][]string {
 			continue
 		}
 		row = strings.Split(s, "")
-		seatmap = append(seatmap, row)
+		seatmap[index] = row
+		index++
 	}
 	return seatmap
 }
@@ -38,5 +84,15 @@ func main() {
 
 	seatmap := parseInput()
 
-	fmt.Printf("part 1: %v\n", seatmap)
+	fmt.Printf("part 1\n")
+	fmt.Printf("input: \n")
+	for i := 0; i < len(seatmap); i++ {
+		fmt.Printf("%v\n", seatmap[i])
+	}
+
+	seatmap = simulate(seatmap)
+	fmt.Printf("\n simulate: \n")
+	for i := 0; i < len(seatmap); i++ {
+		fmt.Printf("%d %v\n", i, seatmap[i])
+	}
 }
