659f1cf1e54ff6eb711aa051fd660e5e7f52ca89
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-19 16:15:32 +0100

    parsing input and rules

---
+++ 19/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var rules = make(map[int]string)
+var input []string
+
+func parseInput() {
+	file, err := os.Open("tiny.txt")
+	if err != nil {
+		fmt.Printf("error opening file\n")
+	}
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+	cursor := "rules"
+
+	for scanner.Scan() {
+		s := scanner.Text()
+
+		if s == "" {
+			// switch to input
+			cursor = "input"
+			fmt.Printf("switching to %s\n", cursor)
+			continue
+		}
+
+		switch cursor {
+		case "rules":
+			parts := strings.Split(s, ":")
+			fmt.Printf("parts: %v\n", parts)
+			index, _ := strconv.Atoi(parts[0])
+			rules[index] = parts[1]
+		case "input":
+			input = append(input, s)
+		}
+	}
+}
+
+func main() {
+	fmt.Printf("hello, day 19\n")
+
+	parseInput()
+	fmt.Printf("parsed input %v\n parsed rules: \n", input)
+	for key, value := range rules {
+		fmt.Printf("[%d]: %s\n", key, value)
+	}
+}
