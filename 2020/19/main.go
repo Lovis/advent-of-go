1a1e68bd6fb54cfe959c2357c285868fb1e80cb0
Lovis Dahl <lovi.dahl@gmail.com>
2020-12-19 17:04:55 +0100

    working for test case

--- 2020/19/main.go
+++ 2020/19/main.go
@@ -11,8 +11,31 @@ import (
 var rules = make(map[int]string)
 var input []string
 
-func testInput(s string) bool {
-	for 
+func getRule(rule int) string {
+	fmt.Printf("get rule: %d len(%d)\n", rule, len(rules[rule]))
+	var subs []string
+	if len(rules[rule]) == 1 {
+		// assume a single character
+		fmt.Printf("got char; returning\n")
+		return rules[rule]
+	}
+	// handle sub
+	subrules := strings.Split(rules[rule], " | ")
+	// 0: 1 2
+	// 1: "a"
+	// 2: 1 3 | 3 1
+	// 3: "b"
+	// 0 => a + ab|ba => aab | aba
+	for i, subrule := range subrules {
+		subs = append(subs, "")
+		ruleSet := strings.Split(subrule, " ")
+		for _, subsub := range ruleSet {
+			fmt.Printf("subrule %d: subsub [%s]\n", i, subsub)
+			next, _ := strconv.Atoi(subsub)
+			subs[i] += getRule(next)
+		}
+	}
+	return strings.Join(subs, "|")
 }
 
 func parseInput() {
@@ -37,10 +60,10 @@ func parseInput() {
 
 		switch cursor {
 		case "rules":
-			parts := strings.Split(s, ":")
+			parts := strings.Split(s, ": ")
 			fmt.Printf("parts: %v\n", parts)
 			index, _ := strconv.Atoi(parts[0])
-			rules[index] = parts[1]
+			rules[index] = strings.Trim(parts[1], "\"")
 		case "input":
 			input = append(input, s)
 		}
@@ -53,6 +76,10 @@ func main() {
 	parseInput()
 	fmt.Printf("parsed input %v\n parsed rules: \n", input)
 	for key, value := range rules {
-		fmt.Printf("[%d]: %s\n", key, value)
+		fmt.Printf("[%d]: %s, len(%d)\n", key, value, len(value))
+
 	}
+
+	// fmt.Printf("len(rules[5]): %d\n", len(rules[5]))
+	fmt.Printf("getRule(5) %s\n", getRule(3))
 }
